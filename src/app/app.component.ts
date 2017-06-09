0ae19db4e082b925f6b6b7633b014d4912a835f5
Arthur <arthurxaviergiffoni@gmail.com>
2017-06-09 08:37:11 -0300

    adding model task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'APP XAVIER';
+
+  task: Task = new Task(21,'Enviar orçamento para o cliente X');
+  task2: Task = new Task(25,'Enviar orçamento para o cliente G');
 }
+
+export class Task{
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
+}
+
