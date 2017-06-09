6c5f7c95db37d18d8c143792e45006f9fb84b005
Arthur <arthurxaviergiffoni@gmail.com>
2017-06-09 12:58:12 -0300

    Adding tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { UsersComponent } from "./users/users.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+
+const USERS: Array<any> = [
+  { id: 1, email: 'test@teste.com'},
+  { id: 2, email: 'test1@teste.com'},
+  { id: 3, email: 'test2@teste.com'},
+  { id: 4, email: 'test3@teste.com'},
+  { id: 5, email: 'test4@teste.com'},
+  { id: 6, email: 'test5@teste.com'},
+  { id: 7, email: 'test6@teste.com'},
+  { id: 8, email: 'test7@teste.com'},
+
+];
+
+@Component({
+  selector: 'users',
+  templateUrl: './users.component.html'
+})
+export class UsersComponent implements OnInit{
+  public users;
+  
+  public constructor(){
+  }
+  
+  public ngOnInit(){
+    this.users = USERS;
+  }
+
+}
